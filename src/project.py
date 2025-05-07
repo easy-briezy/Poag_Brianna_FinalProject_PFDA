72e2dbdfac96de7d50b59ad67eb01da8c1d71c88
Brianna <bgp230001@utdallas.edu>
2025-05-07 14:39:18 -0500

    starter pseudocode

---
+++ src/project.py
@@ -0,0 +1,31 @@
+#import pillow and pygame
+
+#main
+
+#pyagme
+    #screen (images I made)
+    #placement of clickable icons
+    #mouse controls
+    #full screen 
+    #exit button
+
+#store images
+    #clothes list?
+    #hair list?
+
+#image changer/loader
+    #clothes changer
+    #hair changer
+#image placement
+
+#mouse click icon choice
+#the clicked choice is inputed into the matching item category image changer
+
+
+
+
+
+
+
+
+#call main
