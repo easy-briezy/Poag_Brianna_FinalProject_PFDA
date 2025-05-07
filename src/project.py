8180f3fc26d20f99458a3e2627b3df5b8bd7fbe7
Brianna <bgp230001@utdallas.edu>
2025-05-07 15:38:13 -0500

    loads base image (but not properly fitted to screen)

--- src/project.py
+++ src/project.py
@@ -1,17 +1,44 @@
 #import pillow and pygame
+#import pillow
+import pygame
 
-#main
+def main():
 
-#pyagme
+
+    #pyagme
     #screen (images I made)
     #placement of clickable icons
     #mouse controls
     #full screen 
-    #exit button
+    pygame.init()
 
-#store images
+    width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
+    width = max(width, 1920)
+    height = max(height, 1080)
+    screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
+
+    pygame.display.set_caption('Dress Up')
+
+        #exit button
+
+    #store images
+        #bg image
+    base_image = pygame.image.load('images/test_base.png')
     #clothes list?
     #hair list?
+
+    #loop for base image bg
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        #make base
+        screen.blit(base_image, (0,0))
+        pygame.display.flip()
+
+    pygame.quit()
 
 #image changer/loader
     #clothes changer
@@ -29,3 +56,5 @@
 
 
 #call main
+if __name__ == "__main__":
+    main()
