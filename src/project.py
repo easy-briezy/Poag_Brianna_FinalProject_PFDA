10eedf6010651f960a16b50b360b09a163189c58
Brianna <bgp230001@utdallas.edu>
2025-05-11 16:48:20 -0500

    made a way to exit the game and make buttons on screen

--- src/project.py
+++ src/project.py
@@ -1,12 +1,72 @@
 #import pillow and pygame
 from PIL import Image
 import pygame
+#import button
 
+#def store_images():
+   # base_image = pygame.image.load('images/test_base.png').convert_alpha()
+
+    #clothes list
+    #dress =  pygame.image.load('images/clothes/dress_test.png').convert_alpha()
+    #suit = pygame.image.load('images/clothes/suit_test.png').convert_alpha()
+
+    #hair list
+    #curly = pygame.image.load('images/hair/curly_hair_test.png').convert_alpha()
+    #straight = pygame.image.load('images/hair/straight_hair_test.png').convert_alpha()
+
+#screen
+screen_width = 800 #make into constant
+screen_height = 600 #make into a constant not in main
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Dress Up')
+
+exit_image  = pygame.image.load('images/exit.png').convert_alpha()
+
+    
+
+
+base_image = pygame.image.load('images/test_base.png').convert_alpha()
+
+#clothes list
+dress =  pygame.image.load('images/clothes/dress_test.png').convert_alpha()
+suit = pygame.image.load('images/clothes/suit_test.png').convert_alpha()
+
+#hair list
+curly = pygame.image.load('images/hair/curly_hair_test.png').convert_alpha()
+straight = pygame.image.load('images/hair/straight_hair_test.png').convert_alpha()
+
+class Button():
+    def __init__(self, x, y, image, scale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
+
+    def draw(self, screen):
+        results = False
+        pos = pygame.mouse.get_pos() #where mouse is on screen
+
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                results = True
+
+        if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+            self.clicked = True
+            results = True
+
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+        return results
+
+
+
+exit_icon =  Button(100, 500, exit_image, 0.1)
 
 
 def main():
-
-    
     
     #pyagme
     #screen (images I made)
@@ -15,13 +75,6 @@ def main():
     #full screen 
     pygame.init()
 
-    #screen
-    screen_width = 800
-    screen_height = 600
-    screen = pygame.display.set_mode((screen_width, screen_height))
-    
-
-    pygame.display.set_caption('Dress Up')
 
 
         #exit button
@@ -33,14 +86,13 @@ def main():
     
 
     #load and store each image option
-    base_image = pygame.image.load('images/test_base.png').convert_alpha()
-    dress =  pygame.image.load('images/clothes/dress_test.png').convert_alpha()
-    suit = pygame.image.load('images/clothes/suit_test.png').convert_alpha()
-    curly = pygame.image.load('images/hair/curly_hair_test.png').convert_alpha()
-    straight = pygame.image.load('images/hair/straight_hair_test.png').convert_alpha()
+
 
     running = True
     while running:
+
+
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -48,7 +100,11 @@ def main():
         
         
         screen.blit(base_image, (0,0))
-        screen.blit(clothes_picker(clothes_choice), (0,0))
+
+        if exit_icon.draw(screen):
+            print('EXIT')
+            running = False
+        #screen.blit(clothes_picker(clothes_choice), (0,0))
         pygame.display.update()
     
 
@@ -64,8 +120,14 @@ def main():
 #mouse click icon choice
 #the clicked choice is inputed into the matching item category image changer
 
-def clothes_picker(clothes_choice):
-    
+#def clothes_picker(clothes_choice):
+    #dress
+    #suit
+
+#def hair_picker(hair_choice):
+    #curly
+    #straight
+
 
 
 
